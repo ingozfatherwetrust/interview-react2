0962fd9d837b78122ded77bc05e33427df614cee
C.J. Gozdor <kelseyluibrand@Kelseys-MacBook-Pro.local>
2019-12-24 14:19:23 -0500

    added some framework for redux

---
+++ autobooks/src/redux-store/sagas.ts
@@ -0,0 +1,9 @@
+import {all} from 'redux-saga/effects';
+import baseMain from "../base/sagas/base";
+
+export default function * main() {
+    const sagas = [
+        baseMain()
+    ];
+    yield all(sagas)
+}
